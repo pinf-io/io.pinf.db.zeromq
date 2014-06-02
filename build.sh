21f57d0f778b5a050e4c7fe5a6e77d16f138e8fb
Christoph Dorn <christoph@christophdorn.com>
2014-06-02 12:45:45 -0700

    initial

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# TODO: Don't install globally?
+
+if [ ! -e "/usr/local/lib/libzmq.so" ]; then
+	sudo apt-get install -y --force-yes make libtool autoconf automake uuid-dev build-essential
+
+	wget http://download.zeromq.org/zeromq-4.0.3.tar.gz
+	tar zxvf zeromq-4.0.3.tar.gz && cd zeromq-4.0.3
+	./configure
+	make
+	sudo make install
+else
+	echo "libzmq already installed based on found: /usr/local/lib/libzmq.so"
+fi
